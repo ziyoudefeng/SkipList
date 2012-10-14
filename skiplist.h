87085382b720a5a3129edfc29fb5998676a0633f
HaizhuShao <shaohaizhu88@126.comcd>
2012-10-14 21:17:01 +0800

    add funciton's commit

--- skiplist.h
+++ skiplist.h
@@ -4,6 +4,7 @@
 
 #define SKIPLIST_MAXLEVEL 8
 
+// 具体数据的类型
 typedef value_type double;
 
 typedef struct _skiplistNode {
@@ -20,15 +21,24 @@ typedef struct _skiplist {
 	unsigned int level;
 } skiplist;
 
-skiplistNode *slCreateNOde(unsigned int level, value_type score);
+// 创建一个节点的函数，在创建跳表的函数slCreate和插入元素的函数_slInsert中要使用
+skiplistNode *_slCreateNode(unsigned int level, value_type score);
+// 创建跳表
 skiplist *slCreate(void);
+// 删除一个节点，在删除跳表中某个元素时要用，是具体进行释放内存操作的函数
 void _slFreeNode(skiplistNode *);
+// 释放跳表占用的内存空间
 void slFree(skiplist *);
-unsigned int slRandomLevel(void);
-skiplistNode *_slInsert(skiplist *, value_type score);
+unsigned int _slRandomLevel(void);
+// 插入一个元素到跳表中
+skiplistNode *slInsert(skiplist *, value_type score);
+// 删除跳表中某个元素的内部操作函数
 void _slDeleteNode(skiplist *, skipistNode *, skiplistNode **);
+// 删除跳表中==某值的元素
 int slDelete(skiplist *, value_type);
+// 查找元素，找到返回1，否则返回0
 int slSearch(skiplist *, value_type);
+// 打印
 void slPrint(skiplist *);
 
 #endif
@@ -41,4 +51,5 @@ void slPrint(skiplist *);
 #define _INCLUDE_STDIO.H
 #include <stdio.h>
 #endif
+
 
